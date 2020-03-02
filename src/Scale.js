7c4ae93902ce099110291607271f7c7284b229f8
Leonard Magyar <gcb16177@uni.strath.ac.uk>
2020-03-02 17:14:11 +0000

    panel fix

--- src/Scale.js
+++ src/Scale.js
@@ -24,17 +24,17 @@ export default class Scale extends React.Component {
         }
         else{
             timeDiff = 3;
-            segment = <Segment inverted style={{background: "#243665", height:"100%", width:"30vw"}}>
+            segment = <Segment inverted style={{background: "#243665", height:"100%", width:"26.5vw"}}>
                 <Header inverted>
                     <Header.Content style = {{color: "deepskyblue"}}>Travel times in minutes</Header.Content>
                 </Header>
-                <Grid  columns="equal" style={{ width: '28vw', height:"100%"}} padded>
-                    <Grid.Column style = {{background:'navy'}}>0 - {timeDiff}</Grid.Column>
-                    <Grid.Column style = {{background:'royalblue'}}>{timeDiff} - {timeDiff*2}</Grid.Column>
-                    <Grid.Column style = {{background:'seagreen'}}>{timeDiff*2} - {timeDiff*3}</Grid.Column>
-                    <Grid.Column color = "yellow">{timeDiff*3} - {timeDiff*4}</Grid.Column>
-                    <Grid.Column style = {{background:'darkorange'}}>{timeDiff*4} - {timeDiff*5}</Grid.Column>
-                    <Grid.Column style = {{background:'maroon'}}>{timeDiff*5}+</Grid.Column>
+                <Grid  style={{height:"100%"}} padded>
+                    <Grid.Column style = {{background:'navy', width:'3.75vw'}}>0 - {timeDiff}</Grid.Column>
+                    <Grid.Column style = {{background:'royalblue', width:'3.75vw'}}>{timeDiff} - {timeDiff*2}</Grid.Column>
+                    <Grid.Column style = {{background:'seagreen', width:'3.75vw'}}>{timeDiff*2} - {timeDiff*3}</Grid.Column>
+                    <Grid.Column style = {{width:"4.5vw"}} color = "yellow">{timeDiff*3} - {timeDiff*4}</Grid.Column>
+                    <Grid.Column style = {{background:'darkorange', width:'5vw'}}>{timeDiff*4} - {timeDiff*5}</Grid.Column>
+                    <Grid.Column style = {{background:'maroon', width:'3.75vw'}}>{timeDiff*5}+</Grid.Column>
                 </Grid>
             </Segment>
         }
